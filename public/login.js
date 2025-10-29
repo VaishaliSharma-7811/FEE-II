232368c4b3f3d5b685d158e06136ee76e5ccff53
simran-011 <simrananand910@gmail.com>
2025-10-29 15:49:05 +0530

    added validation to login

--- public/login.js
+++ public/login.js
@@ -4,6 +4,27 @@ async function handleLogin(event) {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
+    if(!username) {
+        alert(`username is required`);
+        return;
+    }
+
+    if(username.length<3) {
+        alert(`username should be of atleast 6 characters`);
+        return;
+    }
+
+    if(!password) {
+       alert(`password is required`);
+       return;
+
+    }
+
+    if(password.length<3) {
+        alert(`password should be of atleast 6 characters`);
+        return;
+    }
+
     const res = await fetch(`http://localhost:3001/users?username=${username}&password=${password}`);
     const users = await res.json();
 
